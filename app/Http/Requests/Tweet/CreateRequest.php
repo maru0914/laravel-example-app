c8ef8de4d345f189c2f9cfc6278d46de70a28e46
Takuya Ozu <tozu@DESKTOP-TV2R8UA.localdomain>
2023-04-09 12:10:24 +0900

    [MODIFY] setting to Tweet's CreateRequest

--- app/Http/Requests/Tweet/CreateRequest.php
+++ app/Http/Requests/Tweet/CreateRequest.php
@@ -11,7 +11,7 @@ class CreateRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,7 @@ class CreateRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'tweet' => 'required|max:140'
         ];
     }
 }
