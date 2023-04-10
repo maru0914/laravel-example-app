b0ec2634062fdeb896130a6780057eb60b755bf0
Takuya Ozu <littleoduozu@gmail.com>
2023-04-11 08:38:55 +0900

    [MODIFY] add tweet creating process

--- app/Http/Controllers/Tweet/CreateController.php
+++ app/Http/Controllers/Tweet/CreateController.php
@@ -3,7 +3,8 @@
 namespace App\Http\Controllers\Tweet;
 
 use App\Http\Controllers\Controller;
-use \App\Http\Requests\Tweet\CreateRequest;
+use App\Http\Requests\Tweet\CreateRequest;
+use App\Models\Tweet;
 
 class CreateController extends Controller
 {
@@ -12,6 +13,9 @@ class CreateController extends Controller
      */
     public function __invoke(CreateRequest $request)
     {
-        //
+        $tweet = new Tweet();
+        $tweet->content = $request->tweet();
+        $tweet->save();
+        return redirect()->route('tweet.index');
     }
 }
