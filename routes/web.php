e5a896098023cba8ac1acbf3d7643f1388cdad39
Takuya Ozu <littleoduozu@gmail.com>
2023-04-11 21:45:26 +0900

    [MODIFY] add routing for tweet's update

--- routes/web.php
+++ routes/web.php
@@ -30,3 +30,7 @@ Route::get('/tweet', \App\Http\Controllers\Tweet\IndexController::class)
     ->name('tweet.index');
 Route::post('/tweet/create', \App\Http\Controllers\Tweet\CreateController::class)
     ->name('tweet.create');
+Route::get('/tweet/update/{tweetId}', \App\Http\Controllers\Tweet\IndexController::class)
+    ->name('tweet.update.index');
+Route::put('/tweet/update/{tweetId}', \App\Http\Controllers\Tweet\PutController::class)
+    ->name('tweet.update.put');
