d6b3aefcc779764aef876b50009b256d719a791f
Takuya Ozu <littleoduozu@gmail.com>
2023-04-16 11:36:23 +0900

    [MODIFY] change view of tweet list whether tweet's author or not

--- resources/views/tweet/index.blade.php
+++ resources/views/tweet/index.blade.php
@@ -34,14 +34,18 @@
                 <summary>
                     {{ $tweet->content }} by {{ $tweet->user->name }}
                 </summary>
-                <div>
-                    <a href="{{ route('tweet.update.index', ['tweetId' => $tweet->id]) }}">編集</a>
-                    <form action="{{ route('tweet.delete', ['tweetId' => $tweet->id]) }}" method="post">
-                        @method('DELETE')
-                        @csrf
-                        <button type="submit">削除</button>
-                    </form>
-                </div>
+                @if (\Illuminate\Support\Facades\Auth::id() === $tweet->user_id)
+                    <div>
+                        <a href="{{ route('tweet.update.index', ['tweetId' => $tweet->id]) }}">編集</a>
+                        <form action="{{ route('tweet.delete', ['tweetId' => $tweet->id]) }}" method="post">
+                            @method('DELETE')
+                            @csrf
+                            <button type="submit">削除</button>
+                        </form>
+                    </div>
+                @else
+                    編集できません
+                @endif
             </details>
         @endforeach
     </div>
